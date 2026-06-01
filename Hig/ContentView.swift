90800d3d54b46fa070a3e97377b5b226ba802051
tavinml <gustavomls911@gmail.com>
2026-06-01 08:30:27 -0300

    Initial Commit

---
+++ Hig/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hig
+//
+//  Created by Gustavo Monteiro Lopes on 01/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hig/HigApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigApp.swift
+//  Hig
+//
+//  Created by Gustavo Monteiro Lopes on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
