d9ec14364f8d15346cf94c92b9f57840993fbf2b
bear-a13 <bernardo.saraujo11@gmail.com>
2026-06-03 15:20:58 -0300

    tela slide e barra de progresso

---
+++ Hig/Slide.swift
@@ -0,0 +1,18 @@
+//
+//  Slide.swift
+//  Hig
+//
+//  Created by Bernardo on 02/06/26.
+//
+
+import SwiftUI
+
+struct Slide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Slide()
+}
---
+++ Hig/componentes/Baloon.swift
@@ -0,0 +1,18 @@
+//
+//  Baloon.swift
+//  Hig
+//
+//  Created by Bernardo on 03/06/26.
+//
+
+import SwiftUI
+
+struct Baloon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Baloon()
+}
