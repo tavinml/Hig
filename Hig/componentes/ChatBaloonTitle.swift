e7d14cdcc39a7bfcbed30f9e5da8517dfdcf13b1
Ana14Soares <ana.soarex14@gmail.com>
2026-06-17 11:41:30 -0300

    Bug cmponente do chat

--- Hig/componentes/ChatBaloonTitle.swift
+++ Hig/componentes/ChatBaloonTitle.swift
@@ -19,12 +19,14 @@ struct ChatBaloonTitle: View {
             Rectangle()
                 .fill(.colorLayout)
                 .frame(width: windowWidth > 1200 ? 40 : 30,
-                       height: windowWidth > 1200 ? 35 : 30)
+                       height: windowWidth > 1200 ? 30 : 25)
             
-            HStack(spacing: 6) {
+            HStack(alignment: .center) {
+               
                 if let icon {
                     Image(systemName: icon)
                         .foregroundStyle(iconColor)
+                        .font(windowWidth > 1200 ? .title : .title2)
                 }
                 
                 Text(text)
@@ -33,12 +35,17 @@ struct ChatBaloonTitle: View {
                     .padding(windowWidth > 1200 ? 4 : 2)
                     .background(Color.colorLayout)
                     .cornerRadius(windowWidth > 1200 ? 24 : 18)
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 12)
-                    .background(.colorLayout)
                     .cornerRadius(24)
             }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
+            .background(.colorLayout)
+            .cornerRadius(24)
             .frame(maxWidth: 440, alignment: .leading)
+//            .padding(.horizontal, 20)
+//            .padding(.vertical, 12)
+            
+            
            
         }
     }
